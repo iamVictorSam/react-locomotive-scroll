3f14901715bc6d31e67e0e88b142e64435afbea1
IamVictorSam <iamvictorsam@gmail.com>
2022-08-31 23:47:21 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Introduction from './components/intro/Introduction'
+import Introduction from './components/intro/Introduction';
+import About from './components/about/About';
+import footer from './components/footer/footer';
 
 function App() {
 
   return (
     <div className="">
       <Introduction/>
+      <About/>
+      <footer/>
     </div>
   )
 }
